39da492b756f5ee00cccb754254123410eb48d2b
u-d13 <13.undead@gmail.com>
2011-10-29 00:16:14 +0300

    fixed an 'extra hundred' bug in num2text.js, discovered a 10^3^x bug; updated TODO

--- num2text.js
+++ num2text.js
@@ -1,91 +1,105 @@
-var n = '1988';
-var iInv, i, iMod3;
+var n = '1 2 3 00000 9 00 1 2';
+var i, iMod3, iInv;
 
 var digit = '';
 var nText = '';
 
 console.log(n);
 
-for (i = 0; i < n.length; i++)
-	if (n[i] != ' ') nText += n[i];
+for (i = 0; i < n.length; i++) if (n[i] != ' ') nText += n[i];
 n = nText;
 nText = '';
 
-for (iInv = 1; iInv <= n.length; iInv++) {
+for (iInv = 1; iInv < n.length + 1; iInv++) {
 	i = n.length - iInv;
 	iMod3 = iInv % 3;
 
 	if (iMod3 == 2) {
-		if (n[i] == '1' && n[i+1] != '0') {
+		if (n[i] == '1' && n[i + 1] != '0') {
 			continue;
-		}else{
-			digit = switchTens (n[i]);
+		} else {
+			digit = switchTens(n[i]);
 		}
-	} else {	
+	} else {
 		if (iMod3 && i > 0 && n[i - 1] == '1') {
-			digit = switchTeens (n[i]);
+			digit = switchTeens(n[i]);
 		} else {
-			digit = switchOnes (n[i]);
-		}
-		digit += digit && ' ';
-		if (iMod3) {
-			switch (Math.floor(iInv / 3)) {
-			case 1: digit += 'thousand'; break;
-			case 2: digit += 'million'; break;
-			case 3: digit += 'billion'; break;
+			digit = switchOnes(n[i]);
+			if (digit) {
+				digit += ' '
 			}
-		} else {
-			digit += 'hundred' + (nText && ' and');
+			if (iMod3) {
+				digit += switchThousands(iInv);
+			} else if (digit) {
+				digit += 'hundred';
+				if (nText) {
+					digit += ' and'
+				};
+			}
 		}
 	}
 	if (digit) {
-		nText = digit + ' ' + nText;
+		if (nText) {
+			nText = ' ' + nText;
+		}
+		nText = digit + nText;
 		digit = '';
 	}
 }
+
 console.log(nText);
 
-function switchOnes (n){
+function switchOnes(n) {
 	switch (n) {
-	case '1': return 'one';
-	case '2': return 'two';
-	case '3': return 'three';
-	case '4': return 'four';
-	case '5': return 'five';
-	case '6': return 'six';
-	case '7': return 'seven';
-	case '8': return 'eight';
-	case '9': return 'nine';
-	default: return '';
+	case '0':return '';
+	case '1':return 'one';
+	case '2':return 'two';
+	case '3':return 'three';
+	case '4':return 'four';
+	case '5':return 'five';
+	case '6':return 'six';
+	case '7':return 'seven';
+	case '8':return 'eight';
+	case '9':return 'nine';
 	}
 }
 
-function switchTeens (n){
+function switchTens(n) {
 	switch (n) {
-	case '1': return 'eleven';
-	case '2': return 'twelve';
-	case '3': return 'thirteen';
-	case '4': return 'fourteen';
-	case '5': return 'fifteen';
-	case '6': return 'sixteen';
-	case '7': return 'seventeen';
-	case '8': return 'eighteen';
-	case '9': return 'nineteen';
-	default: return '';
+	case '0':return '';
+	case '1':return 'ten';
+	case '2':return 'twenty';
+	case '3':return 'thirty';
+	case '4':return 'fourty';
+	case '5':return 'fifty';
+	case '6':return 'sixty';
+	case '7':return 'seventy';
+	case '8':return 'eighty';
+	case '9':return 'ninety';
 	}
 }
 
-function switchTens (n){
+function switchTeens(n) {
 	switch (n) {
-	case '1': return 'ten';
-	case '2': return 'twenty';
-	case '3': return 'thirty';
-	case '4': return 'fourty';
-	case '5': return 'fifty';
-	case '6': return 'sixty';
-	case '7': return 'seventy';
-	case '8': return 'eighty';
-	case '9': return 'ninety';
-	default: return '';
+	case '0':return '';
+	case '1':return 'eleven';
+	case '2':return 'twelve';
+	case '3':return 'thirteen';
+	case '4':return 'fourteen';
+	case '5':return 'fifteen';
+	case '6':return 'sixteen';
+	case '7':return 'seventeen';
+	case '8':return 'eighteen';
+	case '9':return 'nineteen';
+	}
+}
+
+function switchThousands(i) {
+	switch (Math.floor(i / 3)) {
+	case 0:	return '';
+	case 1:	return 'thousand';
+	case 2:	return 'million';
+	case 3:	return 'billion';
+	case 4:	return 'trillion';
 	}
 }
